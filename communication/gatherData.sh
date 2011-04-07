72614929892eef6fa6319f8659e5b21501624dda
Dustin Dalen <devnull@localhost>
2011-04-07 12:17:54 -0700

    Adding a minor tweak to the header for the output file info.txt from gatherData.sh. Also making the delimiters between trials consistent for either algorithm.

--- communication/gatherData.sh
+++ communication/gatherData.sh
@@ -30,7 +30,7 @@ if [ $# -ne $NUM_ARGS ]; then
         exit
 fi
 if [ -d ../$NAME ]; then
-        echo "A test named: $3, has already been made. ABORTING"
+        echo "A test named: $NAME, has already been made. ABORTING"
         exit
 fi
 
@@ -40,7 +40,7 @@ touch ../$NAME/info.txt
 echo "Sorting algorith is: $ALGORITHM" >> ../$NAME/info.txt
 echo "Map is: $MAP" >> ../$NAME/info.txt
 echo "Number of trials: $NUM_TRIALS" >> ../$NAME/info.txt
-echo "Description of trial $NAME :" > ../$NAME/info.txt
+echo "Description of trial $NAME :" >> ../$NAME/info.txt
 
 vi ../$NAME/info.txt
 
@@ -64,7 +64,7 @@ for (( j = 0 ; j < $NUM_TRIALS ; j++ )); do
 		tail -n51 temp | head -n50 > temp
 		head -n1 temp | cut -d' ' -f7 >> ../$NAME/results.txt
 		tail -n49 temp | cut -d' ' -f11 >> ../$NAME/results.txt
-		echo 'end run' >> ../$NAME/results.txt
+		echo '*********************************' >> ../$NAME/results.txt
 	else
 		grep "Goal" temp > temp2
 		head -n1 temp2 | cut -d' ' -f6 >> ../$NAME/results.txt
