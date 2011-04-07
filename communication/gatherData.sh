a4fd1af166598880a1707eff034e5c191ef3fa68
Dustin Dalen <devnull@localhost>
2011-04-07 12:10:30 -0700

    Making changes to gatherData.sh Now the usage for gatherData.sh is: gatherData <ziggurat/mccallum> <map> <nsim> <nameOfTrial> The delimiter between trials is now: ************************* The info.txt file created has some basic information already provided for ease of use.
    The main change in this revision is the addition of specifying which algorithm to use and making the output correct for either algorithm.

--- communication/gatherData.sh
+++ communication/gatherData.sh
@@ -11,31 +11,37 @@
 ## Author: Dustin Dalen
 ## Date modified: 4/5/2011
 
-NUM_ARGS=3
+NUM_ARGS=4
 
 #arg1 is the map
 #arg2 is how many trials
 #arg3 is the anme of the trial
 
+#variables
+ALGORITHM=$1
+MAP=$2
+NUM_TRIALS=$3
+NAME=$4
+
+
 if [ $# -ne $NUM_ARGS ]; then
-        echo 'USAGE: gatherData <map> <nsim> <nameOfTrial>'
-	echo '!!must be in the communication folder of upbot!!'
+        echo 'USAGE: gatherData <ziggurat/mccallum> <map> <nsim> <nameOfTrial>'
+		echo '!!must be in the communication folder of upbot!!'
         exit
 fi
-if [ -d ../$3 ]; then
+if [ -d ../$NAME ]; then
         echo "A test named: $3, has already been made. ABORTING"
         exit
 fi
 
-#variables
-MAP=$1
-NUM_TRIALS=$2
-NAME=$3
-
 #necessary file attributes
 mkdir ../$NAME
 touch ../$NAME/info.txt
+echo "Sorting algorith is: $ALGORITHM" >> ../$NAME/info.txt
+echo "Map is: $MAP" >> ../$NAME/info.txt
+echo "Number of trials: $NUM_TRIALS" >> ../$NAME/info.txt
 echo "Description of trial $NAME :" > ../$NAME/info.txt
+
 vi ../$NAME/info.txt
 
 #for j in {1..10}; do
@@ -43,19 +49,32 @@ for (( j = 0 ; j < $NUM_TRIALS ; j++ )); do
 	#run the program with the right args
 	./unitTestServer.out $MAP &
 	sleep 1
-	./supervisorClient.out localhost > temp
+	if [ "$ALGORITHM" == "ziggurat" ]; then
+		./supervisorClient.out localhost > temp
+	elif [ "$ALGORITHM" == "mccallum" ]; then
+		./mccallumClient.out localhost > temp
+	else
+		echo "learning algorithm: $ALGORITHM not found. EXITING"
+		rm -rf ../$NAME
+		exit
+	fi
 
 	#clean up the text
-	tail -n51 temp | head -n50 > temp
-	head -n1 temp | cut -d' ' -f7 >> ../$NAME/results.txt
-	for i in {1..50}; do
+	if [ "$ALGORITHM" == "ziggurat" ]; then
+		tail -n51 temp | head -n50 > temp
+		head -n1 temp | cut -d' ' -f7 >> ../$NAME/results.txt
 		tail -n49 temp | cut -d' ' -f11 >> ../$NAME/results.txt
-	done
-	echo 'end run' >> ../$NAME/results.txt
-
+		echo 'end run' >> ../$NAME/results.txt
+	else
+		grep "Goal" temp > temp2
+		head -n1 temp2 | cut -d' ' -f6 >> ../$NAME/results.txt
+		tail -n49 temp2 | cut -d' ' -f5 >> ../$NAME/results.txt
+		echo '*********************************' >> ../$NAME/results.txt
+		rm temp2
+	fi
+	
 	#kill the process to start again
-	ps | grep unitTest | head -n1 | sed -e 's/tty.*//' | xargs kill 
+	ps | grep unitTest | head -n1 | sed -e 's/tty.*//' | xargs kill
 
 done
 rm temp
-say done
