fc89ff0edc42914563a8a26385b159a7be6c0753
Timothy Yandl <t.r.yandl@gmail.com>
2011-09-28 21:58:15 -0700

    wrote an initial implementation of each method in saccFilt.c (have not tested for compilation or runtime errors) Also added filterCommand to SaccFilt.java. Currently filterCommand is dummied up to just return it's parameter with no change.

--- supervisor/SaccFilter.java
+++ supervisor/SaccFilter.java
@@ -1,110 +1,148 @@
-
+/* Filename: SaccFilter.c
+ * Authors:	Michael Liedtke, Tim Yandl, Andrew Nuxoll
+ * Created: 26 Sep 2011
+ * Last Modified: 28 Sep 2011
+ *
+ * TODO 
+ *
+ **/
 
 public class SaccFilter
 {
-   //private int[] sensors; //currently unused
+    private char[] sensorArray;
 
-   public static void main(String[] args) {} // unused
-   
-   public SaccFilter() {} //nothing needs to be initilized yet.
-   
-   public int[] runFilter(int[] sensors)
-   {
-      return reverseArray(sensors);
-   }
-   
-   private int[] reverseArray(int[] intarr)
-   {
-      for(int i=0; i<intarr.length/2; i++)
-      {
-         int temp = intarr[intarr.length-(i+1)];
-         intarr[intarr.length-(i+1)] = intarr[i];
-         intarr[i] = temp;
-      }
-      return intarr;
-   }
-   
-   /**
-	 * currentWindow()
-	 * Takes an input array and divides it into windows with the specified size.
-	 * It returns the window number corresponding to current window
-	 * 
-	 * @param input: input binary array
-	 * @param windowSize: size of windows to break up the input array into
-	 * @param currentWindow: window number to return
-	 * @return The divided input with a binary label appended to the front
-	 * returns null if the requested window is out of bounds or input is null
-	 */
-	private char[] currentWindow(char[] input, int windowSize, int currentWindow){
-	//returns null if the input is null
-	if(input == null){return null;}
-	//finding the number of windows that the input will be divided into
-	int numWindows = input.length/windowSize + input.length%windowSize; 
-	//returns null if the requested window is out of bounds or if the window size is too long/short
-	if(currentWindow > numWindows-1 || currentWindow < 0 || windowSize < 1 || 
-			windowSize > input.length){return null;}
-	//finds the length of; the binary label that will be appended to the front of the array		
-	int binaryLength = numWindows/2 + numWindows%2;
-	//Sets up a double array to store the divided input
-	char[][] dividedInput = new char[numWindows][binaryLength+windowSize];
-	//keeps track of where in the input array the loop left off at
-	int arrayLocation = 0;
-	//temporary string used to store a binary string
-	String binaryLabel;
-	char[] temp;
-	//assigns all the positions in the divided input array
-	for(int i=0; i<numWindows; i++){
-		//retrieves the binary string from the helper method
-		binaryLabel = getBinary(i,binaryLength);
-		temp = binaryLabel.toCharArray();
-		//sets the front of the array to the binary label
-		for(int k=0; k<binaryLabel.length();k++){
-			dividedInput[i][k] = temp[k];
-		}
-		//saves the input into the divided array in the appropriate place
-		for(int j=0; j<+windowSize;j++){
-			if(arrayLocation<input.length){
-				dividedInput[i][j+binaryLabel.length()] = input[arrayLocation];
-				arrayLocation++;
-			}
-			//if at the end of the input array, insert 0's
-			else
-				dividedInput[i][j+binaryLabel.length()] = '0';
-		}
-	}
-	char[] returnArray = new char[input.length];
-	//sets the values in the return array
-	for(int i=0; i<input.length;i++){
-		if(i<dividedInput[currentWindow].length)
-			returnArray[i]=dividedInput[currentWindow][i];
-		else
-			returnArray[i]='0';
-	}
-	return returnArray;
+    /**
+    * The constructor will set the sensor array to all zeros.
+    * it is important to initialize the sensor array in the event
+    * that the calling program were to immediately ask for the current
+    * sensor array.
+    */
+    public SaccFilter()
+    {
+        char[] sensorArray = {'0','0','0','0','0','0','0','0','0','0'};
+    }
+
+    /**
+    * runFilter is used as platform to launch the filter.
+    * we choose to use it as opposed to calling the filter methods directly
+    * because it allows us to code multiple versions of the filter and call
+    * one at a time without modifying the calling program or changing method names.
+    */
+    public char[] runFilter(char[] sensors)
+    {
+        return reverseArray(sensors);
+    }
+    
+    public int filterCommand(int command)
+    {
+        return command;
+    }
+
+    /**
+    * Just returns the current sensor array.
+    */
+    public char[] getSensorArray()
+{
+        return sensorArray;
+    }
+
+    /**
+    * This method is for testing purposes only!!!
+    * it takes a character array, reverses it,
+    * and spits it back out.
+    */
+    private char[] reverseArray(char[] chararr)
+    {
+        for(int i=0; i<chararr.length/2; i++)
+        {
+            char temp = chararr[chararr.length-(i+1)];
+            chararr[chararr.length-(i+1)] = chararr[i];
+            chararr[i] = temp;
+        }
+        return chararr;
+    }
+
+    /**
+     * currentWindow()
+     * Takes an input array and divides it into windows with the specified size.
+     * It returns the window number corresponding to current window
+     * 
+     * @param input: input binary array
+     * @param windowSize: size of windows to break up the input array into
+     * @param currentWindow: window number to return
+     * @return The divided input with a binary label appended to the front
+     * returns null if the requested window is out of bounds or input is null
+     */
+    private char[] currentWindow(char[] input, int windowSize, int currentWindow){
+    //returns null if the input is null
+    if(input == null){return null;}
+    //finding the number of windows that the input will be divided into
+    int numWindows = input.length/windowSize + input.length%windowSize; 
+    //returns null if the requested window is out of bounds or if the window size is too long/short
+    if(currentWindow > numWindows-1 || currentWindow < 0 || windowSize < 1 || 
+        windowSize > input.length){return null;}
+    //finds the length of; the binary label that will be appended to the front of the array
+    int binaryLength = numWindows/2 + numWindows%2;
+    //Sets up a double array to store the divided input
+    char[][] dividedInput = new char[numWindows][binaryLength+windowSize];
+    //keeps track of where in the input array the loop left off at
+    int arrayLocation = 0;
+    //temporary string used to store a binary string
+    String binaryLabel;
+    char[] temp;
+    //assigns all the positions in the divided input array
+    for(int i=0; i<numWindows; i++){
+        //retrieves the binary string from the helper method
+        binaryLabel = getBinary(i,binaryLength);
+        temp = binaryLabel.toCharArray();
+        //sets the front of the array to the binary label
+        for(int k=0; k<binaryLabel.length();k++){
+            dividedInput[i][k] = temp[k];
+        }
+        //saves the input into the divided array in the appropriate place
+        for(int j=0; j<+windowSize;j++){
+            if(arrayLocation<input.length){
+                dividedInput[i][j+binaryLabel.length()] = input[arrayLocation];
+                arrayLocation++;
+            }
+            //if at the end of the input array, insert 0's
+            else
+                dividedInput[i][j+binaryLabel.length()] = '0';
+        }
+    }
+    char[] returnArray = new char[input.length];
+    //sets the values in the return array
+    for(int i=0; i<input.length;i++){
+        if(i<dividedInput[currentWindow].length)
+            returnArray[i]=dividedInput[currentWindow][i];
+        else
+            returnArray[i]='0';
+    }
+    return returnArray;
 }
-	
-	/**
- 	* getBinary()
- 	* takes a number and converts it into binary with the requested amount of bits
- 	* and returns the binary number in a string form to maintain leading 0's
- 	*  
- 	* @param num: number to turn into binary
- 	* @param numBits: number of bits for the binary form
-	 * @return, a string with the binary form of the input number with the 
- 	* specified number of bits. 
- 	*/
- 	private String getBinary(int num, int numBits){
-		String returnString = Integer.toBinaryString(num);
-		//if the binary form of the input number isn't the specified number of bits,
-		//then append 0's in front until the correct length is reached. 
-		for(int i=0; i<numBits; i++){
-			if(returnString.length() == numBits){
-				return returnString;
-			}
-			else{
-				returnString = '0' + returnString;
-			}
-		}
-		return null;	
-	}  
+    
+    /**
+     * getBinary()
+     * takes a number and converts it into binary with the requested amount of bits
+     * and returns the binary number in a string form to maintain leading 0's
+     *  
+     * @param num: number to turn into binary
+     * @param numBits: number of bits for the binary form
+     * @return, a string with the binary form of the input number with the 
+     * specified number of bits. 
+     */
+     private String getBinary(int num, int numBits){
+        String returnString = Integer.toBinaryString(num);
+        //if the binary form of the input number isn't the specified number of bits,
+        //then append 0's in front until the correct length is reached. 
+        for(int i=0; i<numBits; i++){
+            if(returnString.length() == numBits){
+                return returnString;
+            }
+            else{
+                returnString = '0' + returnString;
+            }
+        }
+        return null;
+    }
 }
--- supervisor/saccFilt.c
+++ supervisor/saccFilt.c
@@ -1,7 +1,7 @@
 /* Filename: saccFilt.c
  * Authors:	Michael Liedtke, Tim Yandl, Andrew Nuxoll
  * Created: 20 Sep 2011
- * Last Modified: -
+ * Last Modified: 28 Sep 2011
  *
  * TODO 
  *
@@ -13,13 +13,15 @@
 #include "supervisor.h"
 
 #include <jni.h>
-#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 
 #define SENSOR_LENGTH 10
 
+
+//note: we are aware that we do not destroy the JVM at the end of a successful run.
+// our expiriments indicate that the JVM is automatically freed.
 JNIEnv *env = NULL; //the environmental pointer
 JavaVM *jvm = NULL; //JVM pointer
 
@@ -27,73 +29,84 @@ jclass clid_Filter = NULL; //the calss ID for SaccFilter
 jobject obid_FilterObj = NULL; //the object ID for SaccFilter
 jmethodID mid_initFilter = NULL; //The ID for the constructor
 jmethodID mid_runFilter = NULL; //The ID for the runFilter method
+jmethodID mid_getSensors = NULL; //The ID for the getSensorArray method
 
 /**
 *This method was taken from the example at http://www.codeproject.com/KB/cpp/CJniJava.aspx
-*Basically it sets up the JVM and returns a pointer to the java environment.
+*Basically it sets up the JVM and returns a pointer to the java environment
+*and sets a pointer to the JVM as a side-effect.
 */
 JNIEnv* create_vm(JavaVM ** jvm)
 {
-   JNIEnv *env;
-   JavaVMInitArgs vm_args;
-   JavaVMOption options;
-   options.optionString = "-Djava.class.path=."; //Path to the java source code assuming it is in the same directory...
-   vm_args.version = JNI_VERSION_1_6; //JDK version. This indicates version 1.6
-   vm_args.nOptions = 1;
-   vm_args.options = &options;
-   vm_args.ignoreUnrecognized = 0;
+    JNIEnv *env;
+    JavaVMInitArgs vm_args;
+    JavaVMOption options;
+    options.optionString = "-Djava.class.path=."; //Path to the java source code assuming it is in the same directory...
+    vm_args.version = JNI_VERSION_1_6; //JDK version. This indicates version 1.6
+    vm_args.nOptions = 1;
+    vm_args.options = &options;
+    vm_args.ignoreUnrecognized = 0;
 
-   //if the JVM could not be created, exit now... the program is hopeless.
-   int ret = JNI_CreateJavaVM(jvm, (void**)&env, &vm_args);
-   if(ret < 0)
-   {
-      printf("\nUnable to Launch JVM\n");
-      exit(EXIT_FAILURE);
-   }
-   return env;
+    //if the JVM could not be created, exit now... the program is hopeless.
+    int ret = JNI_CreateJavaVM(jvm, (void**)&env, &vm_args);
+    if(ret < 0)
+    {
+        //If we failed to set up the JVM, abondonship the program is hopeless.
+        printf("--Unable to Launch JVM--\n");
+        exit(EXIT_FAILURE);
+    }
+    return env;
 }
 
 int init_ids()
 {
     //Obtaining Classes
-   clid_Filter = (*env)->FindClass(env, "SaccFilter");
-   
-   //Obtaining Method IDs method signatures can be obtained by the
-   //linux command javap -s -p <java_classname>
-   if (clid_Filter == NULL)
-   {
-      //if the class was not found, there is no point in continuing,
-      //exit with a fauilure code.
-      printf("failed to load class\n");
-      (*jvm)->DestroyJavaVM(jvm);
-      return EXIT_FAILURE;
-   }
-   //consructor ID for SaccFilter
-   mid_initFilter = (*env)->GetMethodID(env, clid_Filter, "<init>", "()V");
-   //runFilter ID
-   mid_runFilter = (*env)->GetMethodID(env, clid_Filter, "runFilter", "([C)[C");
+    clid_Filter = (*env)->FindClass(env, "SaccFilter");
 
-   
-   
-   /************************************************************************/
-   /*      Now we will call the functions using the their method IDs       */
-   /************************************************************************/
-   if(mid_runFilter == NULL || mid_initFilter == NULL)
-   {
-      //if the methods were not found, there is no point in continuing,
-      //exit with a fauilure code.
-      printf("failed to load methods:\n");
-      (mid_runFilter == NULL) ? printf("runFilter\n") : 0;
-      (mid_initFilter == NULL) ? printf("initFilter\n") : 0;
-      
-      (*jvm)->DestroyJavaVM(jvm);
-      return EXIT_FAILURE;
-   }
-   //requests that java make an instance of a SaccFilt object, which we will call runFilter.
-   //the first parameter is the java environment pointer, the second is the class ID we obtained for SaccFilter
-   //the third is the constructor method ID and the last parameter(s) are the arguements for the method
-   //in this case NULL because there are none.
-   obid_FilterObj = (jobject)(*env)->NewObject(env, clid_Filter, mid_initFilter, NULL);
+    //Obtaining Method IDs method signatures can be obtained by the
+    //linux command javap -s -p <java_classname>
+    if (clid_Filter == NULL)
+    {
+        //if the class was not found, there is no point in continuing,
+        //exit with a fauilure code.
+        printf("--failed to load class--\n");
+        (*jvm)->DestroyJavaVM(jvm);
+        return EXIT_FAILURE;
+    }
+    //consructor ID for SaccFilter
+    mid_initFilter = (*env)->GetMethodID(env, clid_Filter, "<init>", "()V");
+    //runFilter ID
+    mid_runFilter = (*env)->GetMethodID(env, clid_Filter, "runFilter", "([C)[C");
+    //filterCommand
+    mid_filterCommand = (*env)->GetMethodID(env, clid_Filter, "filterCommand", "(I)I");
+    //getSensorArray ID
+    mid_getSensors = (*env)->GetMethodID(env, clid_Filter, "getSensorArray", "()[C");
+
+
+
+    /************************************************************************/
+    /*      Now we will call the functions using the their method IDs       */
+    /************************************************************************/
+    if(mid_runFilter == NULL || mid_initFilter == NULL
+        || mid_getSensors == NULL)
+    {
+        //if the methods were not found, there is no point in continuing,
+        //exit with a fauilure code.
+        printf("--failed to load method(s): ");
+        if(mid_runFilter == NULL){printf(" runFilter");}
+        if(mid_initFilter == NULL){printf(" initFilter");}
+        if(mid_filterCommand == NULL){printf(" filterCommand");}
+        if(mid_getSensors == NULL){printf(" getSensors");}
+        printf("--\n");
+
+        (*jvm)->DestroyJavaVM(jvm);
+        return EXIT_FAILURE;
+    }
+    //requests that java make an instance of a SaccFilt object, which we will call runFilter.
+    //the first parameter is the java environment pointer, the second is the class ID we obtained for SaccFilter
+    //the third is the constructor method ID and the last parameter(s) are the arguements for the method
+    //in this case NULL because there are none.
+    obid_FilterObj = (jobject)(*env)->NewObject(env, clid_Filter, mid_initFilter, NULL);
 }
 
 /* 
@@ -103,15 +116,42 @@ int init_ids()
  */
 char * saccReceiveState(char * input)
 {
+    //Setup the JVM if it isn't already running.
     if (env == NULL)
     {
         env = create_vm(&jvm);
         init_ids();
     }
     
+    //create a jcharArray from the jchar[].
+    jcharArray jca; //the new jcharArray
+    //requests that java make an array of length SENSOR_LENGTH to fit our array
+    jca = (*env)->NewCharArray(env, SENSOR_LENGTH);
+    //copies our array into the jcharArray.
+    (*env)->SetCharArrayRegion(env, jca, 0, SENSOR_LENGTH, input);
     
+    //call the method in Java to run the saccfilter and return a thinned array.
+    //the array returned will need to be converted back into a C-friendly form
+    //before we can pass it back out to the calling program.
+    jcharArray out = (jcharArray)(*env)->CallObjectMethod(env, obid_FilterObj, mid_runFilter, jca);
     
-    return input;
+    jchar * tempArray = (*env)->GetCharArrayElements(env, out, NULL);
+    
+    //check that the extraction was successful
+    if (tempArray == NULL) {
+        return; /* exception occurred */
+    }
+    
+    //this memory will need to be free'd somewhere else...
+    char* ret = (char*)malloc(SENSOR_LENGTH * sizeof(char));
+    
+    int i;
+    for(i=0; i<SENSOR_LENGTH; i++)
+    {
+        ret[i] = (char)tempArray[i];
+    }
+    
+    return ret;
 }
 
 
@@ -122,8 +162,9 @@ char * saccReceiveState(char * input)
  */
 int saccReceiveAction(int command)
 {
+    int ret = (*env)->CallIntMethod(env, obid_FilterObj, mid_filterCommand, (jint)command);
     //send command to the environment
-    return command;
+    return ret;
 }
 
 
@@ -135,5 +176,25 @@ int saccReceiveAction(int command)
  */
 void saccGetCurrSensing(char *buf)
 {
-
+    //gets sensor array by calling the method in java. then must extract the array
+    //from the jcharArray. we now have an array of jchars which needs to be cast
+    //element by element to chars just in-case other parts of the program don't appreciate jchars...
+    jcharArray out = (jcharArray)(*env)->CallObjectMethod(env, obid_FilterObj, mid_getSensors, NULL);
+    int js = (int)(*env)->GetArrayLength(env, out);//testing the actual length of the array (as java sees it)
+    jchar * tempArray = (*env)->GetCharArrayElements(env, out, NULL);
+    
+    //is *buf alread malloc'ed?
+    
+    //casting each element to char and adding them to *buf.
+    int i;
+    for(i=0; i<SENSOR_LENGTH; i++)
+    {
+        buf[i] = (char)tempArray[i];
+    }
+    return;
 }
+
+
+
+
+
