517ecb1964ad0ac6db8acb8018ae12f00b967c09
Michael Liedtke <liedtke13@up.edu>
2011-10-01 11:52:31 -0700

    Modified the currentWindow method in SaccFilter.java to return the window and binary label to the back of a 10 length array rather than the front of one in order to preserve the goal bit, instead of writing over it.

--- supervisor/SaccFilter.java
+++ supervisor/SaccFilter.java
@@ -24,7 +24,7 @@ public class SaccFilter
      * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
      */
     public static final int NUM_COMMANDS = 0xA;
-    public static final int FIRST_SACC_CMD = 0x9;
+    public static final int RST_SACC_CMD = 0x9;
     public static final int CMD_SACC = 0x9;
     
     
@@ -112,8 +112,12 @@ public class SaccFilter
 
     /**
      * currentWindow()
-     * Takes an input array and divides it into windows with the specified size.
-     * It returns the window number corresponding to current window
+     * Divides the sensorArray into windows of length WINDOW_LENGTH and applies a 
+     * binary label to the front of each of the windows. Each window with its binary
+     * label is then pushed to the back of an array of length SENSOR_LENGTH. To preserve
+     * the goal bit in the origional sensorArray, before returning the window, we resave the 
+     * origional goalbit back to array position 0 in our returnArray so that Ziggarat will not
+     * be confused.
      * 
      * @param input: input binary array
      * @param windowSize: size of windows to break up the input array into
@@ -122,52 +126,57 @@ public class SaccFilter
      * returns null if the requested window is out of bounds or input is null
      */
     private char[] currentWindow(/*char[] input, int windowSize, int currentWindow*/){
-    //returns null if the input is null
-    if(sensorArray == null){return null;}
-    //finding the number of windows that the input will be divided into
-    int numWindows = sensorArray.length/WINDOW_SIZE + sensorArray.length%WINDOW_SIZE; 
-    //returns null if the requested window is out of bounds or if the window size is too long/short
-    if(currentWindowAdr > numWindows-1 || currentWindowAdr < 0 || WINDOW_SIZE < 1 || 
-        WINDOW_SIZE > sensorArray.length){return null;}
-    //finds the length of; the binary label that will be appended to the front of the array
-    int binaryLength = numWindows/2 + numWindows%2;
-    //Sets up a double array to store the divided input
-    char[][] dividedInput = new char[numWindows][binaryLength+WINDOW_SIZE];
-    //keeps track of where in the input array the loop left off at
-    int arrayLocation = 0;
-    //temporary string used to store a binary string
-    String binaryLabel;
-    char[] temp;
-    //assigns all the positions in the divided input array
-    for(int i=0; i<numWindows; i++){
-        //retrieves the binary string from the helper method
-        binaryLabel = getBinary(i,binaryLength);
-        temp = binaryLabel.toCharArray();
-        //sets the front of the array to the binary label
-        for(int k=0; k<binaryLabel.length();k++){
-            dividedInput[i][k] = temp[k];
-        }
-        //saves the input into the divided array in the appropriate place
-        for(int j=0; j<+WINDOW_SIZE;j++){
-            if(arrayLocation<sensorArray.length){
-                dividedInput[i][j+binaryLabel.length()] = sensorArray[arrayLocation];
-                arrayLocation++;
-            }
-            //if at the end of the input array, insert 0's
-            else
-                dividedInput[i][j+binaryLabel.length()] = '0';
-        }
+     //returns null if the input is null
+	    if(sensorArray == null){return null;}
+	    //finding the number of windows that the input will be divided into
+	    int numWindows = sensorArray.length/WINDOW_SIZE + sensorArray.length%WINDOW_SIZE; 
+	    //returns null if the requested window is out of bounds or if the window size is too long/short
+	    if(currentWindowAdr > numWindows-1 || currentWindowAdr < 0 || WINDOW_SIZE < 1 || 
+	        WINDOW_SIZE > sensorArray.length){return null;}
+	    //finds the length of; the binary label that will be appended to the front of the array
+	    int binaryLength = numWindows/2 + numWindows%2;
+	    //Sets up a double array to store the divided input
+	    char[][] dividedInput = new char[numWindows][SENSOR_LENGTH];
+	    //keeps track of where in the input array the loop left off at
+	    int arrayLocation = 0;
+	    //temporary string used to store a binary string
+	    String binaryLabel;
+	    char[] temp;
+	    //assigns all the positions in the divided input array
+	    for(int i=0; i<numWindows; i++){
+	        //retrieves the binary string from the helper method
+	        binaryLabel = getBinary(i,binaryLength);
+	        temp = binaryLabel.toCharArray();
+	        //sets the front of the array to the binary label
+	        
+	        int storageLocation = SENSOR_LENGTH-binaryLabel.length()-WINDOW_SIZE;
+	        System.out.println("Bin Length: " + binaryLabel.length() + " stor loc: " +storageLocation );
+	        for(int k=0; k<binaryLabel.length();k++){
+	        	System.out.println("k: " + k);
+	            dividedInput[i][k+storageLocation] = temp[k];
+	        }
+	        //saves the input into the divided array in the appropriate place
+	        for(int j=0; j<WINDOW_SIZE;j++){
+	            if(arrayLocation<sensorArray.length){
+	                dividedInput[i][j+storageLocation+binaryLabel.length()] = sensorArray[arrayLocation];
+	                arrayLocation++;
+	            }
+	            //if at the end of the input array, insert 0's
+	            else
+	                dividedInput[i][j+storageLocation+binaryLabel.length()] = '0';
+	        }
+	    }
+	    char[] returnArray = new char[sensorArray.length];
+	    //sets the values in the return array
+	    for(int i=0; i<SENSOR_LENGTH;i++){
+	        if(i>=binaryLength+WINDOW_SIZE)
+	            returnArray[i]=dividedInput[currentWindowAdr][i];
+	        else
+	            returnArray[i]='0';
+	    }
+	    returnArray[0] = sensorArray[0];
+	    return returnArray;
     }
-    char[] returnArray = new char[sensorArray.length];
-    //sets the values in the return array
-    for(int i=0; i<sensorArray.length;i++){
-        if(i<dividedInput[currentWindowAdr].length)
-            returnArray[i]=dividedInput[currentWindowAdr][i];
-        else
-            returnArray[i]='0';
-    }
-    return returnArray;
-}
     
     /**
      * getBinary()
